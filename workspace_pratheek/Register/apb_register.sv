883b29d0dcce1e82fdd9e73b01693100f9a248ae
svtek <pratheek@svtek.co.in>
2026-05-06 17:17:09 +0530

    Register

--- workspace_pratheek/Register/apb_register.sv
+++ workspace_pratheek/Register/apb_register.sv
@@ -5,6 +5,7 @@ module apb_register #(
     input  logic              i_clk,
     input  logic              i_resetn,
     // Register Interface
+    input  logic              i_reg_enable,
     input  logic              i_reg_wr_en,
     input  logic              i_reg_rd_en,
     input  logic [APB_AW-1:0] i_reg_addr,
@@ -24,58 +25,57 @@ module apb_register #(
     logic [31:0] reg_dev_id_ro;
 
     // Address Decoding and Error Logic
-    logic addr_hit;
-    always_comb begin
-        addr_hit = 1'b0;
-        case (i_reg_addr)
-            16'h0000: addr_hit = 1'b1;
-            16'h0004: addr_hit = 1'b1;
-            16'h0008: addr_hit = 1'b1;
-            16'h000C: addr_hit = 1'b1;
-            16'h0010: addr_hit = 1'b1;
-            16'h0014: addr_hit = 1'b1;
-            16'h0018: addr_hit = 1'b1;
-            default:  addr_hit = 1'b0;
-        endcase
-    end
-
-    logic access_error;
+    logic rd_addr_hit;
+    logic wr_addr_hit;
 
     always_comb begin
-        access_error = 1'b0;
-        if (i_reg_rd_en) begin
-            case (i_reg_addr)
-                16'h0004: access_error = 1'b1; // WO read
-            endcase
-        end
+        rd_addr_hit = 1'b0; // Default to hit, will be cleared if no match
+        wr_addr_hit = 1'b0; // Default to hit, will be cleared if no match
         if (i_reg_wr_en) begin
+            rd_addr_hit = 1'b1; // No read hit during write
             case (i_reg_addr)
-                16'h0018: access_error = 1'b1; // RO write
+                16'h0000: wr_addr_hit = 1'b1;
+                16'h0004: wr_addr_hit = 1'b1;
+                16'h0008: wr_addr_hit = 1'b1;
+                16'h000C: wr_addr_hit = 1'b1;
+                16'h0010: wr_addr_hit = 1'b1;
+                16'h0014: wr_addr_hit = 1'b1;
             endcase
         end
+        else if (i_reg_rd_en) begin
+            wr_addr_hit = 1'b1; // No write hit during read
+            case (i_reg_addr)
+                16'h0000: rd_addr_hit = 1'b1;
+                16'h0008: rd_addr_hit = 1'b1;
+                16'h000C: rd_addr_hit = 1'b1;
+                16'h0010: rd_addr_hit = 1'b1;
+                16'h0014: rd_addr_hit = 1'b1;
+                16'h0018: rd_addr_hit = 1'b1; // RO write
+            endcase
+        end   
     end
 
     // Write Logic
     always_ff @(posedge i_clk or negedge i_resetn) begin
         if (!i_resetn) begin
-            reg_global_ctrl <= 32'h0;
-            reg_command_wo <= 32'h0;
-            reg_status_w1c <= 32'h12345678;
-            reg_sticky_set_w1s <= 32'h0;
-            reg_error_count_rc <= 32'h12345678;
-            reg_polarity_tow <= 32'hFFFFFFFF;
-            reg_dev_id_ro <= 32'h12345678;
+            reg_global_ctrl     <= 32'h0;
+            reg_command_wo      <= 32'h0;
+            reg_status_w1c      <= 32'h12345678;
+            reg_sticky_set_w1s  <= 32'h0;
+            reg_error_count_rc  <= 32'h12345678;
+            reg_polarity_tow    <= 32'hFFFFFFFF;
+            reg_dev_id_ro       <= 32'h12345678;
         end 
-        else if (i_reg_wr_en && addr_hit) begin
+        else if (i_reg_wr_en && i_reg_enable) begin
             case (i_reg_addr)
-                16'h0000: reg_global_ctrl <= i_reg_wdata;
-                16'h0004: reg_command_wo <= i_reg_wdata;
-                16'h0008: reg_status_w1c <= reg_status_w1c & ~i_reg_wdata;
-                16'h000C: reg_sticky_set_w1s <= reg_sticky_set_w1s | i_reg_wdata;
-                16'h0014: reg_polarity_tow <= reg_polarity_tow ^ i_reg_wdata;
+                16'h0000: reg_global_ctrl       <= i_reg_wdata;
+                16'h0004: reg_command_wo        <= i_reg_wdata;
+                16'h0008: reg_status_w1c        <= reg_status_w1c & ~i_reg_wdata;
+                16'h000C: reg_sticky_set_w1s    <= reg_sticky_set_w1s | i_reg_wdata;
+                16'h0014: reg_polarity_tow      <= reg_polarity_tow ^ i_reg_wdata;
             endcase
         end
-        else if (i_reg_rd_en && addr_hit) begin
+        else if (i_reg_rd_en) begin
             if (i_reg_addr == 16'h0010) reg_error_count_rc <= 32'h0;
         end
     end
@@ -83,7 +83,6 @@ module apb_register #(
     // Read Logic
     always_comb begin
         o_reg_rdata = 32'h0;
-        if (i_reg_rd_en) begin
             case (i_reg_addr)
                 16'h0000: o_reg_rdata = reg_global_ctrl;
                 16'h0008: o_reg_rdata = reg_status_w1c;
@@ -93,10 +92,8 @@ module apb_register #(
                 16'h0018: o_reg_rdata = reg_dev_id_ro;
             endcase
         end
-    end
-
     assign o_reg_ready = 1'b1; // Always ready for simplicity
-    assign o_reg_error = (i_reg_rd_en || i_reg_wr_en) && (!addr_hit || access_error);
+    assign o_reg_error = (!rd_addr_hit || !wr_addr_hit);
 
 
 endmodule
